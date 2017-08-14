4901caec0595c42818d24d0d31e2cb2bd3bb5de5
Flux Xu <fluxxu@gmail.com>
2017-08-13 21:00:23 -0400

    WIP

--- cli/src/order.rs
+++ cli/src/order.rs
@@ -4,7 +4,8 @@ use walmart_partner_api::order::*;
 
 pub fn list(client: &Client, status: Option<&str>) {
   let mut query: QueryParams = Default::default();
-  query.createdStartDate = Some(Utc::now() - Duration::hours(24));
+  let start_date = (Utc::now() - Duration::days(7)).date().and_hms(0, 0, 0);
+  query.createdStartDate = Some(start_date);
   // query.createdEndDate = Some(Utc::now());
   query.status = status.or(Some("Created")).map(|s| s.to_owned());
   let res = client.get_all_orders(&query).unwrap();
