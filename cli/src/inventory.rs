54412c3ade73faa59c78cb67f55f25176a932369
ibra <>
2022-12-07 00:19:45 -0500

    feat: update cli to latest clap, use derive and split clients

--- cli/src/inventory.rs
+++ cli/src/inventory.rs
@@ -1,15 +1,97 @@
-use walmart_partner_api::inventory::*;
-use walmart_partner_api::Client;
+use anyhow::Result;
+use clap::{Parser, Subcommand};
 
-pub fn set_inventory(client: &Client, sku: &str, quantity: i32, lagtime: i32) {
-  let inventory = Inventory {
-    quantity: Quantity {
-      unit: "EACH".to_string(),
-      amount: quantity,
-    },
-    sku: sku.to_string(),
-    fulfillmentLagTime: lagtime,
-  };
-  let res = client.update_item_inventory(&inventory).unwrap();
-  println!("{}", serde_json::to_string_pretty(&res).unwrap());
+#[derive(Subcommand)]
+pub enum CaInventoryCommand {
+  Get(Get),
+  Update(CaUpdate),
+}
+
+#[derive(Subcommand)]
+pub enum UsInventoryCommand {
+  Get(Get),
+  Update(UsUpdate),
+}
+
+#[derive(Parser)]
+pub struct Get {
+  #[clap(long)]
+  pub sku: String,
+}
+
+#[derive(Parser)]
+pub struct CaUpdate {
+  #[clap(long)]
+  pub sku: String,
+  #[clap(long)]
+  pub unit: String,
+  #[clap(long)]
+  pub amount: i32,
+  #[clap(long)]
+  pub fulfillment_lag_time: i32,
+  #[clap(long)]
+  pub partner_id: Option<String>,
+  #[clap(long)]
+  pub offer_id: Option<String>,
+}
+
+#[derive(Parser)]
+pub struct UsUpdate {
+  #[clap(long)]
+  pub sku: String,
+  #[clap(long)]
+  pub unit: String,
+  #[clap(long)]
+  pub amount: i32,
+}
+
+impl CaInventoryCommand {
+  pub async fn run(self, client: walmart_partner_api::ca::Client) -> Result<()> {
+    match self {
+      CaInventoryCommand::Get(cmd) => {
+        let r = client.get_item_inventory(cmd.sku).await?;
+        println!("{:#?}", r)
+      }
+      CaInventoryCommand::Update(cmd) => {
+        let r = client
+          .update_item_inventory(walmart_partner_api::ca::Inventory {
+            sku: cmd.sku,
+            fulfillment_lag_time: cmd.fulfillment_lag_time,
+            partner_id: cmd.partner_id,
+            offer_id: cmd.offer_id,
+            quantity: walmart_partner_api::ca::InventoryQuantity {
+              unit: cmd.unit,
+              amount: cmd.amount,
+            },
+          })
+          .await?;
+        println!("{:#?}", r)
+      }
+    }
+    Ok(())
+  }
+}
+
+impl UsInventoryCommand {
+  pub async fn run(self, client: walmart_partner_api::us::Client) -> Result<()> {
+    match self {
+      UsInventoryCommand::Get(cmd) => {
+        let r = client.get_item_inventory(cmd.sku).await?;
+        println!("{:#?}", r)
+      }
+      UsInventoryCommand::Update(cmd) => {
+        let r = client
+          .update_item_inventory(walmart_partner_api::us::Inventory {
+            sku: cmd.sku,
+            quantity: walmart_partner_api::us::InventoryQuantity {
+              unit: cmd.unit,
+              amount: cmd.amount,
+            },
+          })
+          .await?;
+        println!("{:#?}", r)
+      }
+    }
+    Ok(())
+  }
 }
--- cli/src/item.rs
+++ cli/src/item.rs
@@ -1,31 +1,95 @@
-use serde_json;
-use walmart_partner_api::item::*;
-use walmart_partner_api::Client;
+use anyhow::Result;
+use clap::{Parser, Subcommand};
 
-pub fn dump(client: &Client) {
-  let mut params: GetAllItemsQueryParams = Default::default();
-  params.limit = Some(20);
-  let mut items = vec![];
-  loop {
-    println!("loading params = {:#?}", params);
+#[derive(Subcommand)]
+pub enum CaItemCommand {
+  Get(Get),
+  List(List),
+  Retire(Retire),
+}
 
-    let (res, next_params) = client.get_all_items(&params).unwrap();
-    let mut res = res.into_inner();
+#[derive(Subcommand)]
+pub enum UsItemCommand {
+  Get(Get),
+  List(List),
+  Retire(Retire),
+}
 
-    if res.items.is_empty() {
-      break;
+#[derive(Parser)]
+pub struct Get {
+  #[clap(long)]
+  pub sku: String,
+}
+
+#[derive(Parser)]
+pub struct List {
+  #[clap(long)]
+  pub next_cursor: Option<String>,
+  #[clap(long)]
+  pub sku: Option<String>,
+  #[clap(long)]
+  pub limit: Option<i32>,
+  #[clap(long)]
+  pub offset: Option<i32>,
+}
+
+#[derive(Parser)]
+pub struct Retire {
+  #[clap(long)]
+  pub sku: String,
+}
+
+impl CaItemCommand {
+  pub async fn run(self, client: walmart_partner_api::ca::Client) -> Result<()> {
+    match self {
+      CaItemCommand::Get(cmd) => {
+        let r = client.get_item(&cmd.sku).await?;
+        println!("{:#?}", r)
+      }
+      CaItemCommand::List(cmd) => {
+        let r = client
+          .get_all_items(walmart_partner_api::ca::GetAllItemsQuery {
+            next_cursor: cmd.next_cursor,
+            sku: cmd.sku,
+            limit: cmd.limit,
+            offset: cmd.offset,
+          })
+          .await?;
+        println!("{:#?}", r)
+      }
+      CaItemCommand::Retire(cmd) => {
+        let r = client.retire_item(&cmd.sku).await?;
+        println!("{:#?}", r)
+      }
     }
-
-    if let Some(next_params) = next_params {
-      params = next_params;
-      println!("page items = {}", res.items.len());
-      items.append(&mut res.items);
-    } else {
-      break;
-    }
+    Ok(())
   }
+}
 
-  println!("totalItems = {}", items.len());
-
-  println!("{}", serde_json::to_string_pretty(&items).unwrap());
+impl UsItemCommand {
+  pub async fn run(self, client: walmart_partner_api::us::Client) -> Result<()> {
+    match self {
+      UsItemCommand::Get(cmd) => {
+        let r = client.get_item(&cmd.sku).await?;
+        println!("{:#?}", r)
+      }
+      UsItemCommand::List(cmd) => {
+        let r = client
+          .get_all_items(walmart_partner_api::us::GetAllItemsQuery {
+            next_cursor: cmd.next_cursor,
+            sku: cmd.sku,
+            limit: cmd.limit,
+            offset: cmd.offset,
+            ..Default::default()
+          })
+          .await?;
+        println!("{:#?}", r)
+      }
+      UsItemCommand::Retire(cmd) => {
+        let r = client.retire_item(&cmd.sku).await?;
+        println!("{:#?}", r)
+      }
+    }
+    Ok(())
+  }
 }
